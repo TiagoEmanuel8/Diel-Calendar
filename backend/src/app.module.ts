20068a573cd874222682d635442679a81e43a56a
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-09-24 17:23:14 -0300

    feat: backend login usuario

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
+import { LoginModule } from './modules/login/login.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, LoginModule],
   controllers: [],
   providers: [],
 })

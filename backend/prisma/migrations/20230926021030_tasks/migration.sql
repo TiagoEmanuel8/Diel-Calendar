6ee20b35ba1558bd12cc947560b8b8a237ac6ff3
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-09-25 23:11:28 -0300

    feat: backend criando model para tasks

---
+++ backend/prisma/migrations/20230926021030_tasks/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE `Tasks` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `state` VARCHAR(191) NOT NULL,
+    `duration` VARCHAR(191) NOT NULL,
+    `tags` VARCHAR(191) NOT NULL,
+    `times` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Tasks` ADD CONSTRAINT `Tasks_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
